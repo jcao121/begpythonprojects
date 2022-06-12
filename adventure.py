ec1b19d5d934aeba86fcdea4d40d36dbf7c52753
jcao121 <jcao121@gmail.com>
2022-06-12 14:39:54 -0700

    Major changes, mostly done need to dummy proof now

--- adventure.py
+++ adventure.py
@@ -1,12 +1,12 @@
-# TODO: uppercase first letter of first_name, figure out how the attack system will work, attack needs to keep changing
+# TODO: uppercase first letter of first_name, figure out how the attack system will work, attack needs to keep changing, add sleep()
 # FIXME: smaughp drops to negative, i want it to end as soon as its 0, need to find a way, need to dummy proof program also and add sleep to make it more of a dialoge
 from time import sleep
 from random import randint
 
 weapons = ["sword", "axe", "spear"]
-
+game_state = ""
 health = 100
-while health != 0:
+while game_state != "over":
     first_name = input("What is your first name? ")
     if first_name != "":
         print(
@@ -16,8 +16,9 @@ while health != 0:
         continue
     sleep(1)
     playing = input("Do you want to go on an adventure? ").lower()[0]
-
-    if playing == "y":
+    if playing == "":  # FIXME: NEED TO FIX ERROR IF U DONT ENTER ANYTHING
+        continue
+    elif playing == "y":
         print(
             "If we are going on an adventure we need a weapon in case something goes wrong, would you like a Sword, Axe or Spear?"
         )
@@ -33,37 +34,37 @@ while health != 0:
         quit()
     else:
         print("You did not enter yes or no")
-
+        break
+    sleep(1)
     print("You pack your things and follow Gandolf")
     sleep(1)
     print("After half a day of traveling you and Gandolf run into Smaug The Dragon!")
     sleep(1)
 
-    fight_response = input("What do you DO? Enter: Fight or Run? ").lower()
-    if fight_response == "fight":
-        smaughp = 25
-        while smaughp > 0:
+    smaughp = 25
+    while smaughp > 0:
+        fight_response = input("What do you DO? Enter: Fight or Run? ").lower()
+        if fight_response == "fight":
             attack = randint(0, 10)
             smaugattack = randint(0, 8)
-            attacksmaug = print(f"You attacked Smaug and it did {attack} damage")
+            print(f"You attacked Smaug and it did {attack} damage")
             smaughp -= attack
             print(f"Smaug has {smaughp} HP left")
             print(f"Smaug retaliates and hits you for {smaugattack} damage")
             health -= smaugattack
             print(f"You have {health} amount of HP left")
-
-            fight_or_flight = input(
-                "Do you want to keep attacking or try to flee? "
-            ).lower()
-            if fight_or_flight == "attack" and smaughp > 0:
-                continue
-            elif fight_or_flight == "flee":
-                health += 0
-                print(
-                    "You tried to flee and Smaug burned you with his flame breath and you die."
-                )
-            else:
+            if (
+                smaughp <= 0
+            ):  # had an error where when smaug hp was perfect 0 it would keep looping, very important to use <= or >=
+                game_state = "over"
                 print(
                     "You have defeated SMAUG, you beg Gandolf to take you back to The Shire and your adventure ends here..."
                 )
-    break
+            else:
+                continue
+        elif fight_response == "run":
+            game_state = "over"
+            print(
+                "You tried to flee and smaug burned you with his flame breath, YOU DIED!"
+            )
+            break

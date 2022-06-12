f75d75570f099073cf44a9cba927ce4313abf1af
jcao121 <jcao121@gmail.com>
2022-06-11 18:47:06 -0700

    adding attack system to game

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 # - Rock Paper Scissors - can use a list to check if user input is either rock paper or scissors can use “continue” to start loop all over if user does not input rock paper or scissors, used too many if statements can just list all the winning options for player1 and put else as comp wins
 
 # - Guessing game - needed to change input to int I couldn't figure out why it keep looping, was looping because input was a string "4" vs 4
+
+# - Adventure game - needed to use nested while loop instead of continue, because continue started it from the top but I wanted it to repeat one question if they added the wrong input
--- adventure.py
+++ adventure.py
@@ -1,5 +1,7 @@
-# TODO:
+# TODO: uppercase first letter of first_name, figure out how the attack system will work, attack needs to keep changing
 # FIXME:
+from time import sleep
+from random import randint
 
 weapons = ["sword", "axe", "spear"]
 
@@ -7,10 +9,13 @@ health = 100
 while health != 0:
     first_name = input("What is your first name? ")
     if first_name != "":
-        print(f"Is it a pleasure to meet you {first_name}")
+        print(
+            f"Is it a pleasure to meet you {first_name}, My name is Gandolf The Grey, Let's get you out of The Shire. "
+        )
     else:
         continue
     playing = input("Do you want to go on an adventure? ").lower()[0]
+
     if playing == "y":
         print(
             "If we are going on an adventure we need a weapon in case something goes wrong, would you like a Sword, Axe or Spear?"
@@ -27,3 +32,16 @@ while health != 0:
         quit()
     else:
         print("You did not enter yes or no")
+    print("You pack your things and follow Gandolf")
+    sleep(1)
+    print("After half a day of traveling you and Gandolf run into a pack of Wargs")
+    sleep(1)
+    fight_response = input("What do you DO? Enter: Fight or Run? ").lower()
+    wargs = 25
+    attack = randint(0,10)
+    if fight_response == "fight":
+        while wargs > 0:
+
+
+
+
